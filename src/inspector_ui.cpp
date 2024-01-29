696272b6b8f4901b017778ad61200b99a996dfbb
guilleatm <guillearnautm@gmail.com>
2024-01-29 18:02:29 +0000

    backup

--- src/inspector_ui.cpp
+++ src/inspector_ui.cpp
@@ -9,11 +9,8 @@ void InspectorUI::Render()
 {
     if (ImGui::Begin(m_window_name, &m_is_open))
     {
-        ImGui::Text("I am the imgui text");
+        
     }
 
     ImGui::End();
-
-
-    ImGui::ShowDemoWindow();
 }
